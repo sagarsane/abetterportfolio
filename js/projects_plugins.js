f90e75107d8377537e01cd32a2bef0b83da3fb7e
Sagar <sagar2217@gmail.com>
2012-09-05 18:27:16 -0400

    Added data for 'About' section for Projects page
    Projects Page now has a little bit of animation (can be better, learning
    along the way :) ) and now has 1st pass at the About Section.
    Next: Experience Page/GitHub Repos?

--- js/projects_plugins.js
+++ js/projects_plugins.js
@@ -13,23 +13,49 @@ if (!(window.console && console.log)) {
 
 // Place any jQuery/helper plugins in here.
 $(document).ready(function() {
+	//$("#project_buttons").scrollbar();
+	$("#project_details").hide();
+	$("#project_repo").hide();
+	$("#project_reco").hide();
+	$("#project_apis").hide();
+	
 	function projectDataModel(){
 		
 		var self = this;
 		self.projects_info = ko.observableArray();
-		
+		//this.projects_info.loading = ko.observable(false);
 		
 		self.get_projects_info = function(){
 			var proj_info;
 			$.getJSON('/project_data', function(data){
 				self.projects_info(data.projects);
 			});
-		}		
+		}
+		
 		self.get_projects_info();
 		
-		self.project_about = ko.observableArray();	
+		self.project_about = ko.observableArray();
+		self.project_name = ko.observableArray();
 		self.getDetails = function(item){
+			$("#project_details").hide();
+			$("#project_repo").hide();
+			$("#project_reco").hide();
+			$("#project_apis").hide();
+			
+			/*****Insert all the detail information **/
 			self.project_about(item.about);
+			self.project_name(item.name);
+			/****************************************/
+			$("#project_name")
+				.hide()
+				.slideDown('slow');
+				$("#sub_project_details").fadeOut(1000,"fast");
+			$("#project_details").show();
+				$("#sub_project_details").fadeIn(3000,'slow');
+			$("#project_repo").show();
+			$("#project_reco").show();
+			$("#project_apis").show();
+			
 		}
 	}
 	
@@ -37,3 +63,6 @@ $(document).ready(function() {
 });
 
 
+var animateAbout = function(){
+	
+}
--- projects.py
+++ projects.py
@@ -6,110 +6,171 @@ def get_project_data():
             "projects" : 
             [
              {
-              "name": "Diverse Routing Algorithms",
+              "name": "Diverse Routing Algorithms (2012)",
               "technology" : "C, Networking",
-              "about" : "Edge Disjoint Algorithm|Vertex Disjoint Algorithm",
+              "about" : [ 
+                         {"pt" : "Implemented two diverse routing algorithms:"},
+                         {"pt" : "Shortest pair edge-disjoint algorithm"},
+                         {"pt" : "Vertex-disjoint algorithm"},
+                         {"pt" : "Language used was C"},
+                         {"pt" : "Project was done in group of 2"}
+                         ],
               "github" : "<url>",
               "linkedin" : "<url>",
               "apis" : "<apis>"
              },
              {
-              "name" : "Reliable Multicast System",
+              "name" : "Reliable Multicast System (2011)",
               "technology" : "C, UDP, Client-Server, Networking",
-              "about" : "For 2",
+              "about" : [ 
+                         {"pt" : "Designed and implemented point-to-multipoint Reliable Transfer System"},
+                         {"pt" : "System was based on UDP"},
+                         {"pt" : "Implemented over a modified version of Selective Repeat Protocol"},
+                         {"pt" : "Language used was C (with multithreading using pthread)"},
+                         {"pt" : "Project was done in group of 4"}
+                         ],
               "github" : "",
               "linkedin" : "<url>",
               "apis" : "<apis>" 
               },
              {
-              "name" : "Web Based Mashup",
+              "name" : "Web Based Mashup (2011)",
               "technology" : "HTML5, JS/jQuery, PHP, APIs",
-              "about" : "For 3",
+              "about" : [ 
+                         {"pt" : "Implemented a Web-based mashup service for searching online"},
+                         {"pt" : "Provided features like SMS notifications, in-browser calling, open chat room"},
+                         {"pt" : "Used following APIs: Google Maps, Search API; Youtube Data API; Tropo SMS API; Phono API; Pusher API"},
+                         {"pt" : "Implemented using PHP/JS-jQuery/HTML5 components"},
+                         {"pt" : "Project was done in group of 3"}
+                         ],
               "github" : "",
               "linkedin" : "<url>",
               "apis" : "<apis>" 
               
               },
              {
-              "name" : "Integrity Attestation",
+              "name" : "Integrity Attestation (2011)",
               "technology" : "C, Java, Distributed Systems",
-              "about" : "For 4",
+              "about" : [ 
+                         {"pt" : "Implemented a system that effectively pin-points malicious nodes in a Web-Services framework"},
+                         {"pt" : "Used the concept of 'Request Duplication'"},
+                         {"pt" : "Implemented for a 3 tiered Client-Server architecture"},                         
+                         {"pt" : "This multi-threaded system was developed using C and Java"},
+                         {"pt" : "Project was done in group of 3"}
+                         ],
               "github" : "",
               "linkedin" : "<url>",
               "apis" : "<apis>" 
               
               },
              {
-              "name" : "Collaborative Drawing Android App",
+              "name" : "Collaborative Drawing Android App (2011)",
               "technology" : "Android, Peer-to-peer",
-              "about" : "For 5",
+              "about" : [ 
+                         {"pt" : "Developed a multi-user collaborative drawing app in android"},
+                         {"pt" : "It had features like 'Live Drawing Updates' on all user devices, erase drawing, point to a part of drawing using a universal cursor"},
+                         {"pt" : "Project was done in group of 3"}
+                         ],
               "github" : "",
               "linkedin" : "<url>",
               "apis" : "<apis>" 
               
               },
              {
-              "name" : "OS Projects",
+              "name" : "OS Projects (2010)",
               "technology" : "C",
-              "about" : "For 6",
+              "about" : [ 
+                         {"pt" : "Implemented following projects in C -"},
+                         {"pt" : "Virtual Memory and Demand Paging"},
+                         {"pt" : "Reader-writer locks"},                         
+                         {"pt" : "CPU Scheduling algorithms"}
+                         ],
               "github" : "",
               "linkedin" : "<url>",
               "apis" : "<apis>" 
               
               },
              {
-              "name" : "Web Based Dashboard",
+              "name" : "Web Based Dashboard (2009-2010)",
               "technology" : "PHP, MySQL, Flex, HTML, XML",
-              "about" : "For 7",
+              "about" : [ 
+                         {"pt" : "Implemented an abstract interface for security assessment of a network or web application"},
+                         {"pt" : "Server hosted varied security tools remotely"},
+                         {"pt" : "The features were: Scheduling, running scans on any tool and reporting results"},                         
+                         {"pt" : "Project Sponsored by: GS Lab Pvt. Ltd. , India"},
+                         {"pt" : "Awarded First Prize at Technical Symposium at PICT and awarded Second Prize for paper presented on the project at BITS, Goa."},                         
+                         {"pt" : "Project was done in group of 4"}
+                         ],
               "github" : "",
               "linkedin" : "<url>",
               "apis" : "<apis>" 
               
               },
              {
-              "name" : "CPLEX Programming",
+              "name" : "CPLEX Programming (2012)",
               "technology" : "C++, ILP Problems",
-              "about" : "For 8",
+              "about" : [ 
+                         {"pt" : "Developed CPLX Programs for solving ILP Problems"},
+                         {"pt" : "This multi-threaded system was developed using C++ and C++ CPLEX Library"},                         
+                         {"pt" : "Project was done in group of 2"}
+                         ],
               "github" : "",
               "linkedin" : "<url>",
               "apis" : "<apis>" 
               
               },
              {
-              "name" : "ChordLT - Distributed Hashing",
+              "name" : "ChordLT - Distributed Hashing (2011)",
               "technology" : "C, Peer-to-peer",
-              "about" : "For 9",
+              "about" : [ 
+                         {"pt" : "Implemented a light version of Chord Distributed Hash table system using TCP"},
+                         {"pt" : "Based on the paper 'Chord: A scalable Peer-to-Peer Lookup Service for Internet Applications .'"},
+                         {"pt" : "Implemented in C"},                         
+                         {"pt" : "Project was done in group of 4"}
+                         ],
               "github" : "",
               "linkedin" : "<url>",
               "apis" : "<apis>" 
               
               },
              {
-              "name" : "Dynamic Visualization Prototype",
+              "name" : "Dynamic Visualization Prototype (2011)",
               "technology" : "LAMP Stack, ActionScript",
-              "about" : "For 10",
+              "about" : [ 
+                         {"pt" : "Developed and maintained a client-server and flash based dynamic visualization prototype for a specific use case"},
+                         {"pt" : "Implemented in Flex, HTML and LAMP stack"},
+                         {"pt" : "Project was a part of part-time job on campus"}
+                         ],
               "github" : "",
               "linkedin" : "<url>",
               "apis" : "<apis>" 
               
               },
              {
-              "name" : "Registration and Searching Tool",
+              "name" : "Student Registration and Searching Tool (2010-2011)",
               "technology" : "PHP, MySQL, HTML",
-              "about" : "For 11",
+              "about" : [ 
+                         {"pt" : "Implemented a simple student registration system in HTML, PHP and MySQL as a part of a project for a department on campus"},
+                         {"pt" : "Provided a searching tool with various search parameters to search students from the database of students generated in the part one explained above"},                         
+                         {"pt" : "This was a Volunteer and individual project"}
+                         ],
               "github" : "",
               "linkedin" : "<url>",
               "apis" : "<apis>" 
               
               },
              {
-              "name" : "Elementary Circuit Algorithms",
+              "name" : "Elementary Circuit Algorithms (2012)",
               "technology" : "C, Networking",
-              "about" : "For 12",
+              "about" : [ 
+                         {"pt" : "Implementation of Elementary Circuit Algorithms, viz., Tiernan's, Tarjan's and Johnson's algorithms to enumerate all simple cycles in a network"},
+                         {"pt" : "Evaluated these algorithms for their running time against network density and number of nodes in the network"},
+                         {"pt" : "Implemented in C"},                         
+                         {"pt" : "Project was done in group of 2"}
+                         ],
               "github" : "",
               "linkedin" : "<url>",
-              "apis" : "<apis>" 
-              
+              "apis" : "<apis>"              
               }
             ]
            }                
